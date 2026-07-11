844a9810b9d10471747d6890be3c09d949e593cf
Dogs-Life-Academy <huschubaywa@gmail.com>
2026-07-11 09:09:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
-const CACHE_NAME = 'dogs-life-v1';
+const CACHE_NAME = 'dogs-life-v2-icons';
 // Only cache the main entry point and manifest. 
 // Removed .tsx files from cache list as they cause 404 errors in some environments.
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-maskable-512.png',
+  './icons/apple-touch-icon.png'
 ];
 
 // Install SW
